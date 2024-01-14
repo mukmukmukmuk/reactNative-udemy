1a0821d730fd6ade83f181c2df769ee88112557e
ksm010825 <ksm010825@naver.com>
2024-01-14 09:31:44 +0900

    2-27까지

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   Button,
   TextInput,
   ScrollView,
+  FlatList
 } from "react-native";
 
 export default function App() {
@@ -17,7 +18,7 @@ export default function App() {
   const addGoalHandler = () => {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
-      enteredGoalText,
+      {text:enteredGoalText,id:Math.random().toString()}
     ]);
     setEnteredGoalText("");
   };
@@ -32,14 +33,17 @@ export default function App() {
         />
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
-      <View style={styles.goalsContainer}>
-        <ScrollView>
-          {courseGoals.map((goal) => (
-            <View key={goal} style={styles.goalItem}>
-              <Text style={styles.goalText}>{goal}</Text>
+      <View  style={styles.goalsContainer}>
+        <FlatList data={courseGoals} 
+        keyExtractor={(item,index) =>item.id }
+        renderItem={(itemData) =>{ 
+          return (
+            <View  style={styles.goalItem}>
+              <Text style={styles.goalText}>{itemData.item.text}</Text>
             </View>
-          ))}
-        </ScrollView>
+          );
+        }}
+        />
       </View>
     </View>
   );
